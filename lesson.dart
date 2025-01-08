2d001b588f734636afadc49b9184bad03201d813
yntymak <kubanychevyntymak21@gmail.com>
2025-01-08 19:00:43 +0600

    lesson 7 set and lisst,method of dart and const with final

--- lesson.dart
+++ lesson.dart
@@ -1,15 +1,21 @@
-void main() {
-  String title = "Hi";
-  double reting = 1.5;
-  int likes = 1212;
-  bool equre = true;
-  double reating_ofAnime = 10.10;
-  reating_ofAnime = 10.3;
-  bool great = true;
-  great = false;
-  print(reating_ofAnime);
+// void main() {
+//   String title = "Hi";
+//   double reting = 1.5;
+//   int likes = 1212;
+//   bool equre = true;
+//   double reating_ofAnime = 10.10;
+//   reating_ofAnime = 10.3;
+//   bool great = true;
+//   great = false;
+//   print(reating_ofAnime);
 
-  final students = <String>["Yiman,Yntymak,Liliya,Yryskeldi"];
-  students.add("Chika");
-  print(students);
+//   final students = <String>["Yiman,Yntymak,Liliya,Yryskeldi"];
+//   students.add("Chika");
+//   print(students);
+// }
+
+void main() {
+  final classes = <String, double>{"ass": 3.2, "yes": 2.9};
+  classes["As"] = 3;
+  print(classes.keys.toList());
 }
