82f10a04274525a2fca2d8c87e2d7da91f9f8750
yntymak <kubanychevyntymak21@gmail.com>
2025-01-08 17:19:44 +0600

    lessson6

--- lesson.dart
+++ lesson.dart
@@ -3,5 +3,13 @@ void main() {
   double reting = 1.5;
   int likes = 1212;
   bool equre = true;
-  print("hi");
+  double reating_ofAnime = 10.10;
+  reating_ofAnime = 10.3;
+  bool great = true;
+  great = false;
+  print(reating_ofAnime);
+
+  final students = <String>["Yiman,Yntymak,Liliya,Yryskeldi"];
+  students.add("Chika");
+  print(students);
 }
